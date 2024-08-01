bc37d403ab1189dce5b02f52a11a67141caa23d8
Vadim <petrov@outlook.com>
2024-08-01 19:00:48 +0200

    Update OpenAiTest.py

--- OpenAiTest.py
+++ OpenAiTest.py
@@ -1,13 +1,6 @@
 from openai import OpenAI
-from dotenv import load_dotenv
-import os
 
-# load dotenv variables
-load_dotenv()
-
-client = OpenAI(
-  api_key=os.getenv("OPENAI_API_KEY"),
-)
+client = OpenAI()
 
 completion = client.chat.completions.create(
   model="gpt-4o-mini",
